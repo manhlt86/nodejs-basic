b1e065c5029a244df3d748a2d449d7c5059c2038
Strong Lee <letranmanh8x@gmail.com>
2023-06-29 17:42:52 +0700

    view engine

---
+++ src/server.js
@@ -0,0 +1,18 @@
+//const express = require('express');
+import express from "express";
+import configViewEngine from "./configs/viewEngine";
+const app = express();
+const port = 3000;
+
+configViewEngine(app);
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
+
+app.get('/about', (req, res) => {
+    res.send('I am Strong Lee!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
