cd9217ca78d490df1cdd7a9094fda025f464fd10
Strong Lee <letranmanh8x@gmail.com>
2023-06-30 15:14:36 +0700

    static files

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 //const express = require('express');
 import express from "express";
 import configViewEngine from "./configs/viewEngine";
+require('dotenv').config();
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 300;
+console.log('>>>> Check port: ', port);
 
 configViewEngine(app);
+
 app.get('/', (req, res) => {
     res.render('index.ejs')
 })
