d40cc8132fc976fb1ddbf000b957c1cdc8dbfa2b
Strong Lee <letranmanh8x@gmail.com>
2023-06-30 16:15:16 +0700

    routing and MVC

---
+++ src/controller/homeController.js
@@ -0,0 +1,9 @@
+
+let getHomepage = (req, res) => {
+    //logic code
+    return res.render('index.ejs');
+}
+
+module.exports = {
+    getHomepage
+}
--- src/server.js
+++ src/server.js
@@ -1,21 +1,19 @@
 //const express = require('express');
 import express from "express";
 import configViewEngine from "./configs/viewEngine";
+import initWebRoute from "./route/web"
+
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 300;
 console.log('>>>> Check port: ', port);
 
+//Set up view engine
 configViewEngine(app);
 
-app.get('/', (req, res) => {
-    res.render('index.ejs')
-})
-
-app.get('/about', (req, res) => {
-    res.send('I am Strong Lee!')
-})
+//init web route
+initWebRoute(app);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
